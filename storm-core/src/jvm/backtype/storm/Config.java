9c34e6d19d2528f1ff82815ab2185148139c1a56
John Hunt <jhunt@ndustrial.io>
2017-06-12 12:46:47 -0400

    Add new configuration option to validator

--- storm-core/src/jvm/backtype/storm/Config.java
+++ storm-core/src/jvm/backtype/storm/Config.java
@@ -42,6 +42,13 @@ import java.util.Map;
  * Spouts.</p>
  */
 public class Config extends HashMap<String, Object> {
+
+    /**
+     * Hostname to use for JMX remote profiling
+     */
+    public static final String STORM_PROFILING_RMI_HOSTNAME = "storm.profiling.rmi.hostname";
+    public static final Object STORM_PROFILING_RMI_HOSTNAME_SCHEMA = String.class;
+
     /**
      * The transporter for communication among Storm tasks
      */
